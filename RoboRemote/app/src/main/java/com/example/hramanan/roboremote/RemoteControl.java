f5a01b3191c2f6a20e61298cd1d6870a7bfade9b
Hari Ramanan <hramanan@Haris-MacBook-Pro.local>
2016-10-19 08:42:08 -0500

    First commit

---
+++ RoboRemote/app/src/main/java/com/example/hramanan/roboremote/RemoteControl.java
@@ -0,0 +1,13 @@
+package com.example.hramanan.roboremote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoteControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_control);
+    }
+}
