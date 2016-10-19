9e23c576b57241506f8914c59f670d216d3aad45
Hari Ramanan <hramanan@Haris-MacBook-Pro.local>
2016-10-19 10:32:42 -0500

    button click listeners added

--- RoboRemote/app/src/main/java/com/example/hramanan/roboremote/RemoteControl.java
+++ RoboRemote/app/src/main/java/com/example/hramanan/roboremote/RemoteControl.java
@@ -2,12 +2,34 @@ package com.example.hramanan.roboremote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class RemoteControl extends AppCompatActivity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_control);
+    }
+
+    public void forwardButtonOnClick(View v)
+    {
+        Toast.makeText(getBaseContext(), "Robot moving forward.", Toast.LENGTH_LONG).show();
+    }
+
+    public void backwardButtonOnClick(View v)
+    {
+        Toast.makeText(getBaseContext(), "Robot moving backward.", Toast.LENGTH_LONG).show();
+    }
+
+    public void rightButtonOnClick(View v)
+    {
+        Toast.makeText(getBaseContext(), "Robot turning right.", Toast.LENGTH_LONG).show();
+    }
+
+    public void leftButtonOnClick(View v)
+    {
+        Toast.makeText(getBaseContext(), "Robot turning left.", Toast.LENGTH_LONG).show();
     }
 }
