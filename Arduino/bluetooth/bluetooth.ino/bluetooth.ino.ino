9f517fa24a0c3af6ca0976e2e58787b6bf5a4a73
Austin Davis <Austin Davis>
2016-10-28 19:27:40 -0500

    Update to test ardiuno bluetooth

--- Arduino/bluetooth/bluetooth.ino/bluetooth.ino.ino
+++ Arduino/bluetooth/bluetooth.ino/bluetooth.ino.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
+#include <SoftwareSerial.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 // Create the motor shield object with the default I2C address
@@ -12,10 +13,18 @@ Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 // You can also make another motor on port M2
 Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 
+SoftwareSerial mySerial(10, 11); // RX, TX
+
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
   Serial.println("Adafruit Motorshield v2 - DC Motor test!");
 
+  mySerial.begin(4800);
+  mySerial.println("Hello, world?");
+  
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
@@ -34,15 +43,32 @@ void setup() {
 }
 
 void loop() {
+
+  if (mySerial.available()) {
+    char c = mySerial.read();
+  }
+
   uint8_t i;
   
   Serial.print("tick");
 
-delay(5000);
-moveFlail();
+//delay(5000);
+//moveFlail();
 
-
+if(c == '0'){
+  move1Foot();
 }
+if(c == '1'){
+  move2Foot();
+}
+if(c == '2'){
+  move3Foot();
+}
+
+
+
+
+
 
 void move1Foot()
 {
