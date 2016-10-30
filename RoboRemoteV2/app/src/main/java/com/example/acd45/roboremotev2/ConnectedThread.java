1c4ea992f13f3bdd0fa0c531c444bf4a2af5ac5d
Austin Davis <Austin Davis>
2016-10-30 15:44:21 -0500

    finished some bluetooth things

---
+++ RoboRemoteV2/app/src/main/java/com/example/acd45/roboremotev2/ConnectedThread.java
@@ -0,0 +1,41 @@
+package com.example.acd45.roboremotev2;
+
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by acd45 on 10/30/2016.
+ */
+
+public class ConnectedThread extends Thread{
+
+    private final BluetoothSocket mmSocket;
+    //private final InputStream mmInStream;
+    private final OutputStream mmOutStream;
+
+    public ConnectedThread(BluetoothSocket socket) {
+        mmSocket = socket;
+        OutputStream tmpOut = null;
+
+        try {
+            tmpOut = socket.getOutputStream();
+        }
+        catch (IOException e) {}
+
+        mmOutStream = tmpOut;
+    }
+
+    public void write(byte[] bytes){
+        try {
+            mmOutStream.write(bytes);
+        } catch(IOException e){}
+    }
+
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) {}
+    }
+}
