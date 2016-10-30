be5b31cc430e2aa2ac995c2b9cf137dcb369f13e
Hari Ramanan <hramanan@Haris-MacBook-Pro.local>
2016-10-30 15:33:48 -0500

    added a formatted button to the layout.

--- RoboRemote/app/src/main/java/com/example/hramanan/roboremote/RemoteControl.java
+++ RoboRemote/app/src/main/java/com/example/hramanan/roboremote/RemoteControl.java
@@ -1,18 +1,32 @@
 package com.example.hramanan.roboremote;
 
 import android.graphics.Canvas;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class RemoteControl extends AppCompatActivity {
+
+    private Button leftBtn;
+    private Button rightBtn;
+    private Button upBtn;
+    private Button downBtn;
+
+    public
 
     protected void onCreate(Bundle savedInstanceState) {
         Canvas canv1 = new Canvas();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_control);
 
+    }
+
+    private void onClickLeft(View v)
+    {
+        Toast.makeText(this, "Rover moving left", Toast.LENGTH_SHORT).show();
     }
 
 
