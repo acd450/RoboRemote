1f03b50cb1a92bd8e8d6bc8c32c995ff8e2febb6
Austin Davis <Austin Davis>
2016-11-01 19:47:58 -0500

    fixing bluetooth

--- RoboRemoteV2/app/src/main/java/com/example/acd45/roboremotev2/RemoteControl.java
+++ RoboRemoteV2/app/src/main/java/com/example/acd45/roboremotev2/RemoteControl.java
@@ -4,137 +4,295 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.SeekBar;
-import android.widget.Toast;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Set;
 import java.util.UUID;
 
 import android.app.Activity;
 import android.media.MediaPlayer;
+import android.widget.Toast;
+
+import static com.example.acd45.roboremotev2.R.*;
+
+
 
 public class RemoteControl extends AppCompatActivity {
 
     private final static int REQUEST_ENABLE_BT = 1;
-    final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    final byte[] commands = new byte[] { (byte) 0, (byte) 1, (byte)2,
+            (byte) 3, (byte) 4, (byte) 5, (byte) 6, (byte) 7, (byte) 8, (byte) 9};
+    int command = 0;
+
     MediaPlayer mp = null;
+    private Context context;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_remote_control);
-        BluetoothSocket btSocket = null;
-        final Button buttonBT = (Button) findViewById(R.id.button_c);   //BlueTooth button
-        final Button buttonF = (Button) findViewById(R.id.button2);     //Forward button
-        final Button buttonB = (Button) findViewById(R.id.button1);     //Backward button
-        final Button buttonL = (Button) findViewById(R.id.button4);     //Left button
-        final Button buttonR = (Button) findViewById(R.id.button);      //Right button
-        final Button buttonAttack = (Button) findViewById(R.id.button3); //Attack 1
-        final Button buttonAttack2 = (Button) findViewById(R.id.button5); //Attack 2
-        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar2);
-        final byte[] commands = new byte[] { (byte) 0, (byte) 1, (byte)2,
-                (byte) 3, (byte) 4, (byte) 5, (byte) 6, (byte) 7, (byte) 8, (byte) 9};
+        setContentView(layout.activity_remote_control);
+
+        final Button buttonBT = (Button) findViewById(id.button_c);   //BlueTooth button
+        final Button buttonF = (Button) findViewById(id.button2);     //Forward button
+        final Button buttonB = (Button) findViewById(id.button1);     //Backward button
+        final Button buttonL = (Button) findViewById(id.button4);     //Left button
+        final Button buttonR = (Button) findViewById(id.button);      //Right button
+        final Button buttonAttack = (Button) findViewById(id.button3); //Attack 1
+        final Button buttonAttack2 = (Button) findViewById(id.button5); //Attack 2
+        final SeekBar seekBar = (SeekBar) findViewById(id.seekBar2);
 
         //mediaplayer
 
 
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-        }
-
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
+//        if (myBluetooth == null) {
+//            // Device does not support Bluetooth
+//        }
+//
+//        if (!myBluetooth.isEnabled()) {
+//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//        }
 
         buttonBT.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                String address = "98:76:B6:00:64:4A";
-                BluetoothDevice dispositivo = mBluetoothAdapter.getRemoteDevice(address);
+//                try {
+//                        ConnectBT();
+////                    System.out.print("got the address ");
+////                    BluetoothDevice dispositivo = mBluetoothAdapter.getRemoteDevice(address);
+////                    System.out.print("hello ");
+////                    btSocket[0] = dispositivo.createInsecureRfcommSocketToServiceRecord(UUID.randomUUID());
+////                    System.out.print("there ");
+////                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+////                    System.out.print("guy");
+////                    btSocket[0].connect();
+////                    System.out.print(" super");
+//                }catch(IOException e){}
             }
         });
 
+            buttonF.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                        if(event.getAction() == MotionEvent.ACTION_UP) {
+                            command = 1;
+                            sendMessage();
+                        }
+                        else if (event.getAction() == MotionEvent.ACTION_CANCEL){
+                            command = 0;
+                            sendMessage();
+                        }
+                    return true;
+                }
+            });
 
-
-        buttonF.setOnClickListener(new Button.OnClickListener() {
-                                       public void onClick(View v) {
-                                           threadBT.write(commands[1]);
-                                       }
-        });
-
-        buttonB.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                threadBT.write(commands[2]);
-            }
-        });
-
-        buttonL.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                threadBT.write(commands[4]);
-            }
-        });
-
-        buttonR.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                threadBT.write(commands[3]);
-            }
-        });
-
-        buttonAttack.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                threadBT.write(commands[5]);
-                mp = MediaPlayer.create(this, raw.uav);
-                mp.start();
-            }
-        });
-
-        buttonAttack2.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                threadBT.write(commands[6]);
-                mp = MediaPlayer.create(this, raw.tangodown);
-            }
-        });
-
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
+        buttonB.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-               if(progress < 34){
-                    threadBT.write(commands[7]);
-               }
-                else if(progress < 67 && progress > 33){
-                    threadBT.write(commands[8]);
-               }
-                else{
-                    threadBT.write(commands[9]);
-               }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public boolean onTouch(View v, MotionEvent event) {
+                    if(event.getAction() == MotionEvent.ACTION_UP) {
+                        command = 2;
+                        sendMessage();
+                    }
+                    else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+                        command = 0;
+                        sendMessage();
+                    }
+                return true;
             }
         });
 
+        buttonL.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                    if(event.getAction() == MotionEvent.ACTION_UP) {
+                        command = 4;
+                        sendMessage();
+                    }
+                    else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+                        command = 0;
+                        sendMessage();
+                    }
+                return true;
+            }
+        });
+
+        buttonR.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                    if(event.getAction() == MotionEvent.ACTION_UP) {
+                        command = 3;
+                        sendMessage();
+                    }
+                    else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+                        command = 0;
+                        sendMessage();
+                    }
+                return true;
+            }
+        });
+
+
+        buttonAttack.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                    if(event.getAction() == MotionEvent.ACTION_UP) {
+                        command = 5;
+                        sendMessage();
+                        mp = MediaPlayer.create(context, R.raw.uav);
+                        mp.start();
+                    }
+                    else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+                        command = 0;
+                        sendMessage();
+                    }
+                return true;
+            }
+        });
+
+        buttonAttack2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                    if(event.getAction() == MotionEvent.ACTION_UP) {
+                        command = 6;
+                        sendMessage();
+                        mp = MediaPlayer.create(context, R.raw.tangodown);
+                        mp.start();
+                    }
+                    else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+                        command = 0;
+                        sendMessage();
+                    }
+                return true;
+            }
+        });
+
+
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (progress < 34) {
+                            command = 7;
+                            sendMessage();
+                        } else if (progress < 67 && progress > 33) {
+                            command = 8;
+                            sendMessage();
+                        } else {
+                            command = 9;
+                            sendMessage();
+                        }
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+
+    }
+
+
+
+}
+
+class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+{
+    private Context context = getApplicationContext();;
+    final BluetoothAdapter myBluetooth = BluetoothAdapter.getDefaultAdapter();
+    String address = "98:76:B6:00:64:4A";
+    private boolean isBtConnected = false;
+    BluetoothSocket btSocket = null;
+
+
+    //System.out.print("hello ");
+    private boolean ConnectSuccess = true; //if it's here, it's almost connected
+    @Override
+    protected void onPreExecute()
+    {
+        //progress = ProgressDialog.show(ledControl.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+    }
+    @Override
+    protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+    {
+        try
+        {
+            if (btSocket == null || !isBtConnected)
+            {
+                BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(UUID.randomUUID());//create a RFCOMM (SPP) connection
+                BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                btSocket.connect();//start connection
+            }
+        }
+        catch (IOException e)
+        {
+            ConnectSuccess = false;//if the try failed, you can check the exception here
+        }
+        return null;
+    }
+    @Override
+    protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+    {
+        super.onPostExecute(result);
+        if (!ConnectSuccess)
+        {
+            msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+            finish();
+        }
+        else
+        {
+            msg("Connected.");
+            isBtConnected = true;
+        }
+        //progress.dismiss();
+    }
+
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+    private void Disconnect()
+    {
+        if (btSocket!=null) //If the btSocket is busy
+        {
+            try
+            {
+                btSocket.close(); //close connection
+            }
+            catch (IOException e)
+            { msg("Error");}
+        }
+        finish(); //return to the first layout
+    }
+
+    private void sendMessage()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write(commands[command]);
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
     }
 
 }
